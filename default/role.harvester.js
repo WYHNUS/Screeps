de6efda3550d78aefa3e347f61c92d08d28c63ef
WYHNUS <wangyanhao1993@163.com>
2016-11-16 04:01:45 +0800

    fix harvester logic for container

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -7,15 +7,21 @@ module.exports = {
         // get all buildings which can be re-energised
         var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || 
-                            structure.structureType == STRUCTURE_SPAWN || 
-                            structure.structureType == STRUCTURE_CONTAINER) 
-                        && (structure.energyCapacity - structure.energy) > 0;
+                    return (structure.structureType === STRUCTURE_EXTENSION || 
+                            structure.structureType === STRUCTURE_SPAWN) 
+                        && (structure.energyCapacity > structure.energy);
                 }
+        });
+        // check if containers are filled
+        var containers = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType === STRUCTURE_CONTAINER)
+                    && (structure.store[RESOURCE_ENERGY] < structure.storeCapacity);
+            }
         });
 
         // check if need there is a need to gather more energy
-        if (targets.length > 0) {
+        if (targets.length > 0 || containers.length > 0) {
 
             if (creep.carry.energy < creep.carryCapacity) {
                 // if multiple exists, harvest second one
@@ -26,23 +32,28 @@ module.exports = {
                 } else if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(sources[0]);
                 }
-            } else 
-            // re-energise the building!
-            if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0]);
-            }
+            } else if (targets.length > 0) {
+                // re-energise the building before container!
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            } else if (containers.length > 0) {
+                if (creep.transfer(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0]);
+                }
+            } 
 
         } else {
-            // check if creep is within 3 tiles of any of the sources
-            for (var i in sources) {
-                if (creep.pos.inRangeTo(sources[i]), 3) {
-                    // instruct creep to move away towards gather point
-                    var result = creep.moveTo(Game.flags[GATHER_WHEN_IDLE_FLAG]);   // 0 if okay
-                    if (result) {
-                        console.log('error: ' + result + ' when moving to: ' + GATHER_WHEN_IDLE_FLAG);
+                // check if creep is within 3 tiles of any of the sources
+                for (var i in sources) {
+                    if (creep.pos.inRangeTo(sources[i]), 3) {
+                        // instruct creep to move away towards gather point
+                        var result = creep.moveTo(Game.flags[GATHER_WHEN_IDLE_FLAG]);   // 0 if okay
+                        if (result) {
+                            console.log('error: ' + result + ' when moving to: ' + GATHER_WHEN_IDLE_FLAG);
+                        }
                     }
                 }
-            }
         }
     }
 };
