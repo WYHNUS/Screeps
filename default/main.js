a6b453687c7422a3a0b907f1062fccf92b6c8908
WYHNUS <wangyanhao1993@163.com>
2016-11-15 14:13:20 +0800

    add builder logic

--- default/main.js
+++ default/main.js
@@ -1,33 +1,65 @@
 // controllers
 var harvesterCtr = require('role.harvester');
 var upgraderCtr = require('role.upgrader');
+var builderCtr = require('role.builder');
 
 // constant
 let SPAWM = 'Spawn1';
 let NUM = {
-    MAX_CREEPS: 5,
-    harvester: 2,
-    upgrader: 3
+    harvester: 1,
+    upgrader: 4,
+    builder: 1
 };
+let CREEP_COST = {
+    MOVE: 50,
+    WORK: 100,
+    CARRY: 50
+}
 
 module.exports.loop = function() {
     // get number of creeps statistic
     var count = {};
+    for (var role in NUM) {
+        count.role = 0;
+    }
     for (var name in Game.creeps) {
         var role = Game.creeps[name].memory.role;
-        if (count[role]) {
-            count[role]++;
+        if (!!count.role) {
+            count.role++;
         } else {
-            count[role] = 1;
+            count.role = 1;
         }
     }
 
-    for (var role in count) {
-        // add creeps if not enough
-        if (count[role] < NUM[role]) {
-            Game.spawns[SPAWM].createCreep([WORK, CARRY, MOVE], undefined, { role: role });
-            break;
-        } 
+    // check if is currently Spawning
+    if (Game.spawns[SPAWM].spawning === null) {
+        for (var role in count) {
+            // add creeps if not enough
+            if (count.role < NUM.role) {
+                console.log(role + ' : ' + count.role + ' ' + NUM.role);
+                // check if spawn has enough energy to create super-creep
+                /*      
+                    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                    hard-code super-creep attributes for now:
+                        2*MOVE + WORK + 2*CARRY
+                    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                */
+                var superCreepCost = 2*CREEP_COST.MOVE + CREEP_COST.WORK + 2*CREEP_COST.CARRY;
+                // build super creep for harvester and upgrader
+                if (role != 'builder' && Game.spawns[SPAWM].energy >= superCreepCost) {
+                    Game.spawns[SPAWM].createCreep([WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: role });
+                    console.log('spawning creep with role: ' + role);
+                    break;
+                } else if ((role === 'harvester' && count.role === 0) || (role === 'builder')) {
+                    // two conditions:
+                    // 1. no harvester present -> HAVE to get more harvester
+                    // 2. need builder
+                    // Game.spawns[SPAWM].createCreep([WORK, CARRY, MOVE], undefined, { role: role });  
+                    console.log('spawning creep with role: ' + role);
+                    break;
+                }
+            }
+        }
     }
 
     // select all creeps
@@ -46,6 +78,9 @@ module.exports.loop = function() {
                     break;
                 case 'upgrader':
                     upgraderCtr.run(creep);
+                    break;
+                case 'builder':
+                    builderCtr.run(creep);
                     break;
                 default:
                     console.log('unhanddled role: ' + creep.memory.role);
