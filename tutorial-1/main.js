aafff12ec32e1878d4a13c712e91de5c88723ef3
WYHNUS <wangyanhao1993@163.com>
2016-12-03 11:54:39 +0800

    revert tutorial changes

--- tutorial-1/main.js
+++ tutorial-1/main.js
@@ -1,3 +1,7 @@
-module.exports.loop = function () {
+var roleHarvester = require('role.harvester');
 
+module.exports.loop = function() {
+	for (var creep of Game.creeps) {
+		roleHarvester.run(creep);
+	}
 }
